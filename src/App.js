bf64c2628b39234879278de57913bb741b291ced
GitNole <123630604+GitNole@users.noreply.github.com>
2023-03-30 12:46:31 +0200

    components setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import { EndScreen } from './Components/EndScreen';
+import { MainMenu } from "./Components/MainMenu";
+import { Quiz } from './Components/Quiz';
+
 
 function App() {
+  const[gameState, setGameAtate] = useState('menu');
+
   return (
     <div className="App">
-      Quiz App
+      <h1>Quiz app</h1>
+      {gameState === 'menu' && <MainMenu />}
+      {gameState === 'quiz' && <Quiz />}
+      {gameState === 'endScreen' && <EndScreen />}
     </div>
   );
 }
