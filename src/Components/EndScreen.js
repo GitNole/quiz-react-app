a3798d8a611c658bdd8a6f2da763298f11b41063
GitNole <123630604+GitNole@users.noreply.github.com>
2023-03-30 17:48:30 +0200

    EndScreen commit
    + added more questions

--- src/Components/EndScreen.js
+++ src/Components/EndScreen.js
@@ -1,9 +1,19 @@
-import { React } from "react";
+import React, { useContext } from "react";
+import { QuizContext } from "../Helpers/Contexts";
+import { Questions } from "../Helpers/QuestionBank";
+
+
 
 export const EndScreen = () => {
+    const { score, setScore, setGameState  } = useContext(QuizContext);
     return (
-        <div>
-            End
+        <div className="EndScreen">
+        <h1>Quiz Finished</h1>
+        <h3>{score} / {Questions.length}</h3>
+        <button onClick={() => {
+            setGameState('menu')
+            setScore(0);
+          }}>Try Again</button>
         </div>
     )    
 }
